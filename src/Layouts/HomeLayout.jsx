95ee3d919b4461dd4decd2fcf7082009ba387f01
Hasan MD Atik <akhasannaeem@gmail.com>
2025-12-02 00:57:38 +0600

    added go top button

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../Components/Footer";
+import { BiArrowFromBottom } from "react-icons/bi";
 
 const HomeLayout = () => {
   return (
@@ -15,6 +16,15 @@ const HomeLayout = () => {
       <footer>
         <Footer></Footer>
       </footer>
+      <a href="#top" aria-label="Go to top">
+        <div className="h-13 bg-green-800 w-13  rounded-full fixed bottom-3 right-3 cursor-pointer flex justify-center items-center  transition-all duration-300">
+          <BiArrowFromBottom
+            size={30}
+            className=" animate-bounce pt-1"
+            color="white"
+          ></BiArrowFromBottom>
+        </div>
+      </a>
     </>
   );
 };
